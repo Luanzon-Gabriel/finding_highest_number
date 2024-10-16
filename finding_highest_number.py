b1b4d73f2f34fd4549f210b5d2a12acb0aec0aab
Gabriel <you@example.com>
2024-10-16 21:47:14 +0800

    pseudo code

---
+++ finding_highest_number.py
@@ -0,0 +1,17 @@
+#FUNCTION find_highest_number(num1, num2, num3, num4, num5)
+    #IF num2 > num1 THEN
+        #SET highest TO num2
+    #ELSE
+        #SET highest TO num1
+
+    #IF num3 > highest THEN
+        #SET highest TO num3
+
+    #IF num4 > highest THEN
+        #SET highest TO num4
+
+    #IF num5 > highest THEN
+        #SET highest TO num5
+
+    #RETURN highest
+#END FUNCTION
